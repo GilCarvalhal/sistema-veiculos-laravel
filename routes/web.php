07ee504ac3ac2036d568248abf0f24d4ea39139e
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-03-23 15:58:49 -0300

    Criação da rota

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+Route::get('/', [VehicleController::class, "listVehicles"])->name("vehicles.list");
 
 Auth::routes();
 
