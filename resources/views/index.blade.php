4f84fa11bd5dc3bde2d3ed180f6df9dae2fb440c
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-03-23 16:06:04 -0300

    Criação da view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">teste 1</div>
+
+            <div class="col-9">teste 2</div>
+        </div>
+    </div>
+@endsection
